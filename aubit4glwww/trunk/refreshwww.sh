1a726ab280b4d92beab818332a155d3a3a673303
afalout <afalout@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2001-09-05 02:12:26 +0000

    C leaning

--- aubit4glwww/trunk/refreshwww.sh
+++ aubit4glwww/trunk/refreshwww.sh
@@ -4,9 +4,33 @@
 #and on-line doccumentation from Aubit CVS on
 #SourceForge.
 #
+#All changes to Aubit web site should be done like this:
+#1) check out "aubit4glwww" and/or "aubit4gldoc" branch from SF CVS
+#2) make your changes on that checked out code/pages/pictures
+#3) commit your local copy to SF CVS
+#4) log in to SF shell account
+#5) execute this script
+#
 #Note: this script is INTERACTIVE, and will prompt you for
-#your SourceForge CVS password: use expect if you need to
+#your (mine, if you don't change my login name - "afalout") SourceForge 
+#CVS password: use "expect" if you need to
 #make it non-interactive
+#
+#You must run this script when logged in SF shell account.
+#
+#Aubit web site on SF shell account is loccated at:
+#/home/groups/a/au/aubit4gl/htdocs
+#
+#Andrej Falout's HOME at SF shell account is located at:
+#/home/users/a/af/afalout
+#
+#TODO: make it non-interactive
+#TODO: create PHP page that will execute non-interactive version
+#   of this script when user click on the link; protect
+#   this page with username/password, using .htaccess file
+#
+#
+#
 #####################################################
 cd $HOME
 cvs -dafalout@cvs1:/cvsroot/aubit4gl co aubit4glwww
