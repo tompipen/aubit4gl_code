5dc57d4da3c280d976346753057e8bd869944494
afalout <afalout@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2002-03-28 01:31:08 +0000

    Fixing makefiles on CygWin after separating RPC

--- aubit4glsrc/trunk/lib/libsql/nosql/nosql.c
+++ aubit4glsrc/trunk/lib/libsql/nosql/nosql.c
@@ -24,7 +24,7 @@
 # | contact afalout@ihug.co.nz                                           |
 # +----------------------------------------------------------------------+
 #
-# $Id: nosql.c,v 1.10 2002-02-19 09:18:34 afalout Exp $
+# $Id: nosql.c,v 1.11 2002-03-28 01:31:08 afalout Exp $
 #*/
 
 /**
@@ -84,7 +84,7 @@ extern long _nm__sqlca;
 #include "libincl/pointers.h"
 #include "libincl/dtypes.h"
 
-extern void exitwith(char *s);
+//extern void exitwith(char *s);
 
 //=====================================================================
 /*
@@ -163,7 +163,7 @@ int
 A4GLSQL_init_connection (char *dbName)
 {
   debug("Connection with database not available: A4GL_SQLTYPE=nosql");
-  exitwith ("Could not init_connection - Connection with database not available: A4GL_SQLTYPE=nosql");
+  local_exitwith ("Could not init_connection - Connection with database not available: A4GL_SQLTYPE=nosql");
 }
 
 int
@@ -278,14 +278,14 @@ or
 int
 A4GLSQL_read_columns (char *tabname, char *colname, int *dtype, int *size)
 {
-	exitwith ("Could not read_columns - noODBC build");
+	local_exitwith ("Could not read_columns - noODBC build");
 
 }
 
 int
 A4GLSQL_make_connection (UCHAR * server, UCHAR * uid_p, UCHAR * pwd_p)
 {
-  //exitwith ("Could not make ODBC connection - noODBC build");
+  //local_exitwith ("Could not make ODBC connection - noODBC build");
   //FIXME: do we want to show some message here????
 
   //FIXME: programs that do not use DATBASE or CONNECT or SESSION,
@@ -296,7 +296,7 @@ A4GLSQL_make_connection (UCHAR * server, UCHAR * uid_p, UCHAR * pwd_p)
 
 int A4GLSQL_get_datatype (char *db, char *tab, char *col)
 {
-	exitwith ("Could not get_datatype - noODBC build");
+	local_exitwith ("Could not get_datatype - noODBC build");
 }
 
 /*
@@ -307,73 +307,73 @@ functions required to create executable from 4gl code that uses SQL
 
 int A4GLSQL_init_session (char *sessname, char *dsn, char *usr, char *pwd)
 {
-  exitwith ("Could not init_session - noODBC build");
+  local_exitwith ("Could not init_session - noODBC build");
 }
 
 int A4GLSQL_set_conn (char *sessname)
 {
-  exitwith ("Could not set_session - noODBC build");
+  local_exitwith ("Could not set_session - noODBC build");
 }
 
 struct s_sid *
 A4GLSQL_prepare_glob_sql (char *s, int ni, struct BINDING *ibind)
 {
-  exitwith ("Could not prepare_global_sql - noODBC build");
+  local_exitwith ("Could not prepare_global_sql - noODBC build");
 }
 
 int
 A4GLSQL_execute_implicit_sql (struct s_sid *sid)
 {
-  exitwith ("Could not execute_implicit_sql - noODBC build");
+  local_exitwith ("Could not execute_implicit_sql - noODBC build");
 }
 
 int A4GLSQL_close_session (char *sessname)
 {
-  exitwith ("Could not close_session - noODBC build");
+  local_exitwith ("Could not close_session - noODBC build");
 }
 
 int
 A4GLSQL_fill_array (int mx, char **arr1, int szarr1, char **arr2, int szarr2,
 		    char *service, int mode, char *info)
 {
-  exitwith ("Could not fill_array - noODBC build");
+  local_exitwith ("Could not fill_array - noODBC build");
 }
 
 struct s_sid *
 A4GLSQL_prepare_sql (char *s)
 {
-  exitwith ("Could not prepare_sql - noODBC build");
+  local_exitwith ("Could not prepare_sql - noODBC build");
 }
 
 int
 A4GLSQL_add_prepare (char *pname, struct s_sid *sid)
 {
-  exitwith ("Could not add_prepare - noODBC build");
+  local_exitwith ("Could not add_prepare - noODBC build");
 }
 
 int A4GLSQL_execute_sql_from_ptr (char *pname, int ni, char **ibind)
 {
-  exitwith ("Could not execute_sql_from_ptr - noODBC build");
+  local_exitwith ("Could not execute_sql_from_ptr - noODBC build");
 }
 
 int
 A4GLSQL_execute_implicit_select (struct s_sid *sid)
 {
-  exitwith ("Could not execute_implicit_select - noODBC build");
+  local_exitwith ("Could not execute_implicit_select - noODBC build");
 }
 
 struct s_sid *
 A4GLSQL_prepare_select (struct BINDING *ibind, int ni, struct BINDING *obind,
 			int no, char *s)
 {
-  exitwith ("Could not prepare_select - noODBC build");
+  local_exitwith ("Could not prepare_select - noODBC build");
 }
 
 struct s_cid *
 A4GLSQL_declare_cursor (int upd_hold, struct s_sid *sid, int scroll,
 			char *cursname)
 {
-  exitwith ("Could not declare_cursor - noODBC build");
+  local_exitwith ("Could not declare_cursor - noODBC build");
 }
 
 int A4GLSQL_set_sqlca_sqlcode (int a)
@@ -385,7 +385,7 @@ int A4GLSQL_set_sqlca_sqlcode (int a)
 int
 A4GLSQL_open_cursor (int ni, char *s)
 {
-  exitwith ("Could not open_cursor - noODBC build");
+  local_exitwith ("Could not open_cursor - noODBC build");
 }
 
 int
@@ -393,39 +393,39 @@ A4GLSQL_fetch_cursor (char *cursor_name,
 		      int fetch_mode, int fetch_when, int nibind,
 		      struct BINDING *ibind)
 {
-  exitwith ("Could not fetch_cursor - noODBC build");
+  local_exitwith ("Could not fetch_cursor - noODBC build");
 }
 
 int A4GLSQL_put_insert (struct BINDING *ibind, int n)
 {
-  exitwith ("Could not put_insert - noODBC build");
+  local_exitwith ("Could not put_insert - noODBC build");
 }
 
 int A4GLSQL_unload_data (char *fname, char *delims, char *sql1)
 {
-  exitwith ("Could not unload_data - noODBC build");
+  local_exitwith ("Could not unload_data - noODBC build");
 }
 
 int A4GLSQL_commit_rollback (int mode)
 {
-  exitwith ("Could not commit_rollback - noODBC build");
+  local_exitwith ("Could not commit_rollback - noODBC build");
 }
 
 struct s_sid *
 A4GLSQL_find_prepare (char *pname, int mode)
 {
-	exitwith ("Could not find_prepare - noODBC build");
+	local_exitwith ("Could not find_prepare - noODBC build");
 }
 
 int A4GLSQL_flush_cursor (char *cursor)
 {
-	exitwith ("Could not flush_cursor - noODBC build");
+	local_exitwith ("Could not flush_cursor - noODBC build");
 }
 
 int
 A4GLSQL_execute_sql (char *pname, int ni, struct BINDING *ibind)
 {
-	exitwith ("Could not execute_sql - noODBC build");
+	local_exitwith ("Could not execute_sql - noODBC build");
 }
 
 /**
@@ -437,4 +437,36 @@ int A4GLSQL_initsqllib()
 {
 	return 1;
 }
+
+
+/**
+ * Exit the program printing a message to the standard output.
+ *
+ * @param s The string to be printed.
+ *
+ * we cannot use "exitwith" directly, because this will create circular dependency on Windows .dll
+ * libaclall -> sql_nosql -> libaclall
+ */
+void local_exitwith (char *s)
+{
+  //mja_endwin ();
+#ifdef DEBUG
+/* {DEBUG} */
+  {
+    debug ("ending because : %s", s);
+  }
+#endif
+  printf ("\n\n\n");
+//  printf (s, sqlca.sqlerrm);
+//  printf ("\n");
+  exit (1);
+//  fgl_end ();
+}
+
+
+
+
+
 //---------------------------- EOF ------------------------------
+
+
