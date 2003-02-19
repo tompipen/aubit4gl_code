2bbe3183d28b4ed9975ad654a726ef2b4d2affec
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2003-02-19 09:54:58 +0000

    .

---
+++ aubit4glsrc/trunk/lib/liblex/lex_c/bad_idents.c
@@ -0,0 +1,12 @@
+#include "a4gl_lib_lex_c_int.h"
+
+
+int bad_identifiers(char *s) {
+        if (strcasecmp(s,"case")==0) return 1;
+        if (strcasecmp(s,"switch")==0) return 1;
+        if (strcasecmp(s,"if")==0) return 1;
+        if (strcasecmp(s,"while")==0) return 1;
+        if (strcasecmp(s,"do")==0) return 1;
+
+return 0;
+}
