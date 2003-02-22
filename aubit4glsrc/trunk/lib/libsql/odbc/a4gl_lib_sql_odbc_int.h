fbdfd03e8afc011799ff0707c31d5423a47cfcfe
afalout <afalout@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2003-02-22 11:06:57 +0000

    Small fixes

--- aubit4glsrc/trunk/lib/libsql/odbc/a4gl_lib_sql_odbc_int.h
+++ aubit4glsrc/trunk/lib/libsql/odbc/a4gl_lib_sql_odbc_int.h
@@ -54,16 +54,16 @@
 
 
 	#ifdef SAPODBC
-			#include "WINDOWS.H"
+			#include "interfaces/odbc/incl/WINDOWS.H"
 			/*
 			incl/WINDOWS.H Header file for non-MS Windows platforms. On MS Windows 3.1 or
 			Windows NT, this file can be replaced by windows.h if an SDK has been installed.
             Also defines thingsa like DWORD needed by followint headers.
 			*/
 
-			#include <sql.h> 		/* Header file for the ODBC driver (Core). */
-			#include <sqlext.h>     /* Header file for the ODBC driver (Level1 and Level2). */
-			#include <sqltypes.h>   /* Header file for the ODBC driver (Datatypes). */
+			#include "interfaces/odbc/incl/sql.h" 		/* Header file for the ODBC driver (Core). */
+			#include "interfaces/odbc/incl/sqlext.h"     /* Header file for the ODBC driver (Level1 and Level2). */
+			#include "interfaces/odbc/incl/sqltypes.h"   /* Header file for the ODBC driver (Datatypes). */
 
 			#define __UCHAR_DEFINED__
 		    #define __ODBC_DEFINED__
