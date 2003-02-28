b36ee2cff3ac32807a668fc5a570a3c62d6b8537
afalout <afalout@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2003-02-28 07:23:02 +0000

    Fix for new 4glc paramters

--- aubit4glsrc/trunk/tools/test/make.bat
+++ aubit4glsrc/trunk/tools/test/make.bat
@@ -1,9 +1,11 @@
-@echo off
+rem @echo off
 fcompile form.per
 mkmess helpfile
-4glc -o  hello.4gl
-4glc -o  hello2.4gl
-4glc -o  hello_db.4gl
+4glc -o hello.exe hello.4gl
+4glc -o hello2.exe hello2.4gl
+4glc -o hello_db.exe hello_db.4gl
 echo.
 echo Example 4gl programs compiled
 echo.
+
+
