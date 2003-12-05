7c4bbd5519de0f1de39d7a1525b1542f6a71434d
afalout <afalout@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2003-12-05 01:44:36 +0000

    Updates

--- aubit4glsrc/trunk/compilers/pcode/compiler_main.c
+++ aubit4glsrc/trunk/compilers/pcode/compiler_main.c
@@ -1,3 +1,43 @@
+/*
+# +----------------------------------------------------------------------+
+# | Aubit 4gl Language Compiler Version $.0                              |
+# +----------------------------------------------------------------------+
+# | Copyright (c) 2000-1 Aubit Development Team (See Credits file)       |
+# +----------------------------------------------------------------------+
+# | This program is free software; you can redistribute it and/or modify |
+# | it under the terms of one of the following licenses:                 |
+# |                                                                      |
+# |  A) the GNU General Public License as published by the Free Software |
+# |     Foundation; either version 2 of the License, or (at your option) |
+# |     any later version.                                               |
+# |                                                                      |
+# |  B) the Aubit License as published by the Aubit Development Team and |
+# |     included in the distribution in the file: LICENSE                |
+# |                                                                      |
+# | This program is distributed in the hope that it will be useful,      |
+# | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+# | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+# | GNU General Public License for more details.                         |
+# |                                                                      |
+# | You should have received a copy of both licenses referred to here.   |
+# | If you did not, or have any questions about Aubit licensing, please  |
+# | contact afalout@ihug.co.nz                                           |
+# +----------------------------------------------------------------------+
+#
+# $Id: compiler_main.c,v 1.7 2003-12-05 01:44:36 afalout Exp $
+#*/
+
+/**
+ * @file
+ *
+ */
+
+/*
+=====================================================================
+		                    Includes
+=====================================================================
+*/
+
 #include <stdio.h>
 
 #if (defined(__CYGWIN__)) || defined(__MINGW32__)
@@ -8,57 +48,145 @@
 
 #include "npcode.h"
 #include "npcode_defs.h"
+#include "time.h"
+
+/*
+=====================================================================
+                    Variables definitions
+=====================================================================
+*/
+
 extern FILE *yyin;
 extern int yydebug;
 
-int yyparse (void);
 int debug_mode = 0;
 
 char fout[256]="a.4pe";
 
 struct module this_module;
 
+/*
+=====================================================================
+                    Functions prototypes
+=====================================================================
+*/
+
+int yyparse (void);
+
+
+/*
+=====================================================================
+                    Functions definitions
+=====================================================================
+*/
+
+
+/**
+ * Parse the comand line arguments and acording to the passed values
+ * set(s) the properties.
+ *
+ * @param argc The argument count
+ * @param argv The argument values
+ */
 int
 main (int argc, char *argv[])
 {
-  int a;
-
-//yydebug=1;
+int a;
+int got_input=0;
+int ignore_next=0;
 
   this_module.fglc_magic = FGLC_XDR_MAGIC;
   this_module.fglc_version = FGLC_XDR_VERSION;
 
-  if (strcmp (argv[1], "-") == 0)
-    {
+	if (argc == 1) {
+		printf("\n");
+		printf("Aubit C to P-code compiler. Usage:\n");
+		printf("\n");
+		printf(" -d : debug mode\n");
+		printf(" -y : debug mode + yydebug\n");
+		printf(" -D -L -I -o -l -c flags are accepted for compatibility with C compiler, but ignored\n");
+		printf("\n");
+		exit (1);
+    }
+
+  if (strcmp (argv[1], "-") == 0) {
       this_module.module_name = "-";
       yyin = stdin;
       strcpy(fout,"a.4pe");
-    }
-  else
-    {
+  } else {
 	char *ptr;
-      this_module.module_name = strdup (argv[1]);
-      yyin = fopen (argv[1], "r");
-      strcpy(fout,argv[1]);
-      ptr=strrchr(fout,'.');
-	if (ptr) {
-		*ptr=0;
-	}
-	strcat(fout,".4pe");
-    }
+	char arg[256];
+    char *c12;
 
-  if (argc > 2)
-    {
-      if (strcmp (argv[2], "-d") == 0)
-	{
-	  debug_mode = 1;
-	}
-      if (strcmp (argv[2], "-D") == 0)
-	{
-	  debug_mode = 1;
-	  yydebug = 1;
-	}
-    }
+
+//printf("argc %d\n",argc);
+//exit (1);
+		for (a = 1; a < argc; a++) {
+//printf("arg=%d\n",a);
+//exit (1);
+		  if (ignore_next == 1) {
+            ignore_next=0;
+			continue;
+          }
+
+		  strcpy(arg,argv[a]);
+          sprintf(c12,"%c%c",arg[0],arg[1]);
+
+ 		  if (strcmp (argv[a], "-d") == 0) {
+			  //printf("got -d\n");
+			  debug_mode = 1;
+		  } else if (strcmp (argv[a], "-y") == 0) {
+			  //printf("got -y\n");
+			  debug_mode = 1;
+			  yydebug = 1;
+		  } else if (strcmp (c12, "-I") == 0) {
+			//printf("got -I\n");
+			// -I flag: ignore
+            continue;
+		  } else if (strcmp (c12, "-L") == 0) {
+			//printf("got -L\n");
+			// -L flag: ignore
+            continue;
+		  } else if (strcmp (c12, "-D") == 0) {
+			//printf("got -D\n");
+			// -D flag: ignore
+            continue;
+		  } else if (strcmp (c12, "-o") == 0) {
+			//printf("got -o\n");
+			// -o flag: ignore
+            ignore_next=1;
+            continue;
+		  } else if (strcmp (c12, "-l") == 0) {
+			//printf("got -l\n");
+			// -l flag: ignore
+            continue;
+		  } else if (strcmp (c12, "-c") == 0) {
+			//printf("got -c\n");
+			// -c flag: ignore
+            continue;
+		  } else {
+			  this_module.module_name = strdup (argv[a]);
+			//printf ("Opening input file %s\n",argv[a]);
+			  yyin = fopen (this_module.module_name, "r");
+			  if (yyin == 0) {
+				printf ("Unable to open input file %s\n",this_module.module_name);
+				exit (2);
+			  } else {
+			      strcpy(fout,this_module.module_name);
+			      ptr=strrchr(fout,'.');
+				  if (ptr) { *ptr=0; }
+				  strcat(fout,".4pe");
+	              got_input=1;
+ 				//yyparse ();
+			  }
+          } //end if
+        } //end for
+  }
+
+  if (got_input == 0) {
+	printf("ERROR: no input file\n");
+	exit (13);
+  }
 
   do_compiler_start (argc, argv);
 
@@ -88,28 +216,27 @@ main (int argc, char *argv[])
     ptr = malloc (1024 * 1024 * 16);
     free (ptr);			// Should speed things up a little...
   }
+
   this_module.external_function_table.external_function_table_len = 0;
   this_module.external_function_table.external_function_table_val = 0;
 
   A4GL_debug ("STring table : %d entries\n",
-	      this_module.string_table.string_table_len);
+  	this_module.string_table.string_table_len);
 
-  for (a = 0; a < this_module.string_table.string_table_len; a++)
-    {
+  for (a = 0; a < this_module.string_table.string_table_len; a++) {
       A4GL_debug ("%d. %s\n", a,
 		  this_module.string_table.string_table_val[a].s);
-    }
+  }
 
 
   add_default_named_structs ();	// For things like BINDING...
 
-  if (yyin)
-    yyparse ();
-  else
-    {
-      printf ("Unable to open input file\n");
-      exit (2);
-    }
+  if (yyin) {
+		yyparse ();
+  } else {
+		printf ("Unable to open input file\n");
+		exit (2);
+  }
 
   end_define_module ();
 
@@ -117,23 +244,19 @@ main (int argc, char *argv[])
   //resolve_gotos ();
   move_defines ();
 
-
   A4GL_debug ("String table has %d entries\n",
 	      this_module.string_table.string_table_len);
-
 
   //print_module();
 
   a = process_xdr ('O', &this_module, fout);
-  if (a)
-    {
+  if (a) {
       A4GL_debug ("Written ok %d\n", a);
-    }
-  else
-    {
+    } else {
       printf ("Failed to write %d\n", a);
       exit (1);
     }
+
   return 0;
 }
 
@@ -160,3 +283,7 @@ call_function (long pc, struct npcmd_call *c)
   printf ("Shouldn't be calling functions during compilation..\n");
   exit (1);
 }
+
+
+/* ================================= EOF ============================== */
+
