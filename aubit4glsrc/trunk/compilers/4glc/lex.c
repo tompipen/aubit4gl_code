a4092d67e385a9654a78eddd8fe1f17657e0112a
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2004-01-23 13:49:49 +0000

    Move the liblex API stuff here from the libaubit4gl stuff

---
+++ aubit4glsrc/trunk/compilers/4glc/lex.c
@@ -0,0 +1,44 @@
+/*******************************************************************
+* (c) 1997-2003 Aubit Computing Ltd.
+*
+*
+********************************************************************/
+
+
+#include "a4gl_libaubit4gl.h"
+/*
+This module is intended to remove any problems with generating liblex from
+the .spec file.
+
+In theory - all API_... calls should call the function that they are called.
+If this is NOT done then we will have problems later on if we want to statically
+link the library rather than using some environment variable to choose one at run
+time....
+*/
+
+
+/* ------------ functions from compile_[c|perl].c -------------- */
+void A4GL_lex_printc(char* fmt,... ) {
+va_list ap;
+   va_start(ap,fmt);
+   A4GL_internal_lex_printc(fmt,&ap);
+   va_end(ap);
+}
+
+/* printh (char *fmt, ...) */
+void A4GL_lex_printh(char* fmt,... ) {
+    va_list ap;
+   va_start(ap,fmt);
+   A4GL_internal_lex_printh(fmt,&ap);
+   va_end(ap);
+}
+
+/* void printcomment (char *fmt,...) */
+void A4GL_lex_printcomment(char* fmt,... ) {
+va_list ap;
+   va_start(ap,fmt);
+   A4GL_internal_lex_printcomment(fmt,&ap);
+   va_end(ap);
+}
+
+
