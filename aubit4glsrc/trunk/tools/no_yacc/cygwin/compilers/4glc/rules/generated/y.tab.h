20a1dba9a0e69fead15e4bc0cfca794e73087621
afalout <afalout@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2005-02-11 12:42:13 +0000

    Small updates

--- aubit4glsrc/trunk/tools/no_yacc/cygwin/compilers/4glc/rules/generated/y.tab.h
+++ aubit4glsrc/trunk/tools/no_yacc/cygwin/compilers/4glc/rules/generated/y.tab.h
@@ -1,7 +1,7 @@
-/* A Bison parser, made by GNU Bison 1.875b.  */
+/* A Bison parser, made by GNU Bison 1.875.  */
 
 /* Skeleton parser for Yacc-like parsing with Bison,
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -2283,8 +2283,8 @@ typedef union YYSTYPE {
 	struct sql_statement *sql_stmt;
 	struct ilist int_list;
 	} YYSTYPE;
-/* Line 1252 of yacc.c.  */
-#line 2288 "y.tab.h"
+/* Line 1248 of yacc.c.  */
+#line 2287 "y.tab.h"
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 # define YYSTYPE_IS_TRIVIAL 1
