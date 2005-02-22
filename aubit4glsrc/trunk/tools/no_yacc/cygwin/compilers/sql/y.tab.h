36f751fa988b05551e510bd50fff5863c21c4de7
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2005-02-22 13:00:03 +0000

    .

--- aubit4glsrc/trunk/tools/no_yacc/cygwin/compilers/sql/y.tab.h
+++ aubit4glsrc/trunk/tools/no_yacc/cygwin/compilers/sql/y.tab.h
@@ -478,7 +478,7 @@
 
 
 #if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
-#line 153 "sql.yacc"
+#line 154 "sql.yacc"
 typedef union YYSTYPE {
         char    str[1024];
    char *sql_string;
