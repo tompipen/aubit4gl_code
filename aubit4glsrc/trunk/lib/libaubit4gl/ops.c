6a8c9c668c28d9bc8e275a441788766c6fb92d5a
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2005-02-23 12:05:42 +0000

    .

--- aubit4glsrc/trunk/lib/libaubit4gl/ops.c
+++ aubit4glsrc/trunk/lib/libaubit4gl/ops.c
@@ -24,7 +24,7 @@
 # | contact afalout@ihug.co.nz                                           |
 # +----------------------------------------------------------------------+
 #
-# $Id: ops.c,v 1.77 2005-02-23 11:44:51 mikeaubury Exp $
+# $Id: ops.c,v 1.78 2005-02-23 12:05:42 mikeaubury Exp $
 #
 */
 
@@ -306,7 +306,15 @@ static void A4GL_char_char_ops (int op)
 
   A4GL_debug("a='%s' b='%s' op=%d\n",a,b,op);
 
-  if (A4GL_isnull (DTYPE_CHAR, (void *) &a) || A4GL_isnull (DTYPE_CHAR, (void *) &b)) { A4GL_push_null (DTYPE_CHAR, 0); free(a); free(b); return; }
+  if (A4GL_isnull (DTYPE_CHAR, (void *) a) || A4GL_isnull (DTYPE_CHAR, (void *) b)) { 
+		//printf("n1=%d",(A4GL_isnull (DTYPE_CHAR, (void *) &a)));
+		//printf("n2=%d",(A4GL_isnull (DTYPE_CHAR, (void *) &b)));
+		//printf("n3=%d",(A4GL_isnull (DTYPE_CHAR, (char *) a)));
+		//printf("n4=%d",(A4GL_isnull (DTYPE_CHAR, (char *) b)));
+		A4GL_push_null (DTYPE_CHAR, 0); 
+		free(a); free(b); 
+		return; 
+	}
   else { A4GL_debug ("OK - neither is null"); } 
 
   A4GL_trim(b);
@@ -369,6 +377,7 @@ static void A4GL_char_char_ops (int op)
       return;
 
     case OP_EQUAL:
+	//if (strcmp(a,"journal")==0 && strcmp(b,"all")==0) { A4GL_assertion(1,"Testing"); }
 	A4GL_debug("Comparing %s %s - gives %d\n",a,b,(strcmp(a,b)==0));
       A4GL_push_int (strcmp(a,b)==0);
 	A4GL_debug("Freeing my two popped strings %p and %p",a,b);
