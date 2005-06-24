2d12fd0bd5e0a60287d3b202c038fc6a2cc60fee
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2005-06-24 13:01:08 +0000

    Increase size of gen_stacks string..

--- aubit4glsrc/trunk/compilers/4glc/gen_stack.h
+++ aubit4glsrc/trunk/compilers/4glc/gen_stack.h
@@ -10,12 +10,13 @@
 #define GEN_STACKS                10
 #define GEN_STACK_SIZE 10000
 
+#define GEN_STACK_ELEM_SIZE 2048
 
 #ifdef GEN_STACK_HERE
-char gen_stack[GEN_STACKS][GEN_STACK_SIZE][800];
+char gen_stack[GEN_STACKS][GEN_STACK_SIZE][GEN_STACK_ELEM_SIZE];
 int gen_stack_cnt[GEN_STACKS] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 #else
-extern char gen_stack[GEN_STACKS][GEN_STACK_SIZE][800];
+extern char gen_stack[GEN_STACKS][GEN_STACK_SIZE][GEN_STACK_ELEM_SIZE];
 extern int gen_stack_cnt[GEN_STACKS] ;
 #endif
 
