17f51c421c7b3afc5752ea5842ff09de3bc79a45
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2005-12-05 21:13:57 +0000

    .

--- aubit4glsrc/trunk/lib/liblex/lex_esqlc/a4gl_lib_lex_esqlc_int.h
+++ aubit4glsrc/trunk/lib/liblex/lex_esqlc/a4gl_lib_lex_esqlc_int.h
@@ -2,5 +2,5 @@
 
 
 #include "a4gl_lib_lex_int.h"
-#define A4GL_append_expr A4GL_append_expr_obsol
-#define A4GL_new_expr A4GL_new_expr_obsol
+//#define A4GL_append_expr A4GL_append_expr_obsol
+//#define A4GL_new_expr A4GL_new_expr_obsol
