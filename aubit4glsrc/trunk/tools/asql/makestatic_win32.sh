98fa10ddb10558028a617ab58d84fbecee6c6b2f
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2006-09-06 12:30:32 +0000

    .

--- aubit4glsrc/trunk/tools/asql/makestatic_win32.sh
+++ aubit4glsrc/trunk/tools/asql/makestatic_win32.sh
@@ -2,7 +2,7 @@ unset DEBUG
 export A4GL_LEXTYPE=EC
 export A4GL_LEXDIALECT=INFORMIX
 export AUBITDIR=c:/gnu/cygwin/usr/src/aubit/aubit4glsrc
-export PATH=$PATH:$AUBITDIR/bin:$AUBITDIR/lib
+export PATH=$PATH:/usr/src/aubit/aubit4glsrc/bin:/usr/src/aubit/aubit4glsrc/lib:$AUBITDIR/bin:$AUBITDIR/lib
 
 cd /usr/src/aubit/aubit4glsrc/lib/libui/ui_curses
 make -f makefile.win32
@@ -17,6 +17,7 @@ then
 4glpc -g -DSTATIC_LIBAUBIT4GL -o ifx_cedit.ao cedit.4gl
 4glpc -g -DSTATIC_LIBAUBIT4GL -o ifx_connection_menu.ao connection_menu.4gl
 4glpc -g -DSTATIC_LIBAUBIT4GL -o ifx_database.ao database.4gl
+4glpc -g -DSTATIC_LIBAUBIT4GL -o ifx_menus.ao menus.4gl
 4glpc -g -DSTATIC_LIBAUBIT4GL -o ifx_execute.ao execute.4gl
 4glpc -g -DSTATIC_LIBAUBIT4GL -o ifx_filehand.ao filehand.4gl
 4glpc -g -DSTATIC_LIBAUBIT4GL -o ifx_form.ao form.4gl
@@ -34,4 +35,4 @@ then
 fi
 
 
-gcc -mms-bitfields -Wl,--export-all-symbols $IFX_LIBS -g  ifx_asql.ao ifx_connection_menu.ao ifx_pick.ao ifx_database.ao ifx_main_menu.ao ifx_query.ao ifx_session.ao ifx_table.ao ifx_execute.ao ifx_paginate.ao ifx_cedit.ao ifx_filehand.ao ifx_load.ao ifx_form.ao ifx_tools.ao ifx_user_menu.ao ifx_report.ao lex.asql_yy.o simple_parse.o dir.o ifx_infx.ao -o asql_si.exe -LC:/gnu/cygwin/usr/src/aubit/aubit4glsrc/lib  ../../lib/libaubit4gl/libaubit4gl.a -Lc:/gnu/cygwin/usr/src/aubit/aubit4glsrc/tools/cygwin/libform -lform_g c:/gnu/pdc27_ming_w32/panel.a c:/gnu/pdc27_ming_w32/pdcurses.a  "C:/PROGRA~1/IBM/Informix/CLIENT~1/bin/isqlt09a.dll" "C:/PROGRA~1/IBM/Informix/CLIENT~1/bin/igl4n304.dll" "C:/PROGRA~1/IBM/Informix/CLIENT~1/bin/iglxn304.dll" "C:/PROGRA~1/IBM/Informix/CLIENT~1/bin/igo4n304.dll"    -lwsock32
+gcc -mms-bitfields -Wl,--export-all-symbols $IFX_LIBS -g  ifx_menus.ao   ifx_asql.ao ifx_connection_menu.ao ifx_pick.ao ifx_database.ao ifx_main_menu.ao ifx_query.ao ifx_session.ao ifx_table.ao ifx_execute.ao ifx_paginate.ao ifx_cedit.ao ifx_filehand.ao ifx_load.ao ifx_form.ao ifx_tools.ao ifx_user_menu.ao ifx_report.ao lex.asql_yy.o simple_parse.o dir.o ifx_infx.ao -o asql_si.exe -LC:/gnu/cygwin/usr/src/aubit/aubit4glsrc/lib  ../../lib/libaubit4gl/libaubit4gl.a -Lc:/gnu/cygwin/usr/src/aubit/aubit4glsrc/tools/cygwin/libform -lform_g c:/gnu/pdc27_ming_w32/panel.a c:/gnu/pdc27_ming_w32/pdcurses.a  "C:/PROGRA~1/IBM/Informix/CLIENT~1/bin/isqlt09a.dll" "C:/PROGRA~1/IBM/Informix/CLIENT~1/bin/igl4n304.dll" "C:/PROGRA~1/IBM/Informix/CLIENT~1/bin/iglxn304.dll" "C:/PROGRA~1/IBM/Informix/CLIENT~1/bin/igo4n304.dll"    -lwsock32
