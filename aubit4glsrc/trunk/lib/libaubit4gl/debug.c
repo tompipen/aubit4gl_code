7c9de6bcf7613af13b0e2447d4d7f533c77cd7a6
gyver309 <gyver309@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2007-02-20 18:43:37 +0000

    Function name logging added - functions A4GL_debug_full_extended(), A4GL_set_line_extended(). Buffer size increased.

--- aubit4glsrc/trunk/lib/libaubit4gl/debug.c
+++ aubit4glsrc/trunk/lib/libaubit4gl/debug.c
@@ -24,7 +24,7 @@
 # | contact afalout@ihug.co.nz                                           |
 # +----------------------------------------------------------------------+
 #
-# $Id: debug.c,v 1.54 2006-10-23 10:07:58 mikeaubury Exp $
+# $Id: debug.c,v 1.55 2007-02-20 18:43:37 gyver309 Exp $
 #
 */
 
@@ -65,7 +65,9 @@ extern sqlca_struct a4gl_sqlca;
 
 FILE *debugfile = 0;
 int nodebug = DEBUG_DONTKNOW;
-static char g_fname[256];
+static char g_fname[1024];
+static char g_function[128];
+static char g_level[64];
 static int g_lineno;
 
 static char arg0[15] = "**undefined**";
@@ -111,6 +113,109 @@ if (strlen(debugfilename)==0) {
       PRINTF ("Unable to open debug.out - check directory permissions...\n");
 	A4GL_fgl_die(2);
     }
+}
+
+/**
+ * The A4GL_debug function.
+ * The function is used because it exist a define that calls
+ * it, if A4GL_debug is set.
+ *
+ */
+void
+A4GL_debug_full_extended (char *fmt, ...)
+{
+  static va_list args;
+#define MAX_DEBUG 10000
+  static char buff[MAX_DEBUG+1];
+  static int a;
+  static int dbg_level=-1;
+  static char buff_n[20];
+  static int indebug=0;
+
+
+  if (nodebug == DEBUG_NOTREQUIRED) { return; }
+
+  if (indebug) return;
+  indebug++;
+
+
+  if (strlen(fmt)==0) {
+		A4GL_assertion(1,"No format for debug");
+		A4GL_pause_execution();
+  }
+
+  if (nodebug == DEBUG_DONTKNOW)
+    {
+      if (strlen (acl_getenv ("DEBUG")))
+	nodebug = DEBUG_REQUIRED;
+      else
+	{
+	  nodebug = DEBUG_NOTREQUIRED;
+	  indebug=0;
+	  return;
+	}
+    }
+
+  if (debugfile == 0)
+    {
+      open_debugfile ();
+    }
+
+  if (isdigit(fmt[0])) {
+	if (!isdigit(fmt[1])) {
+		buff_n[0]=fmt[0];
+		buff_n[1]=0;
+	} else {
+		buff_n[0]=fmt[0];
+		buff_n[1]=fmt[1];
+		buff_n[2]=0;
+	}
+	if (dbg_level==-1)  {
+  		dbg_level=atoi(acl_getenv("DEBUG_LEVEL"));
+	}
+  	a=atoi(buff_n);
+  	if (a && dbg_level && a > dbg_level) {
+		indebug=0;
+		return;
+  	}
+  }
+
+  if (strncmp(g_fname,"API",3)==0) {
+		char buff2[MAX_DEBUG+100];
+		SPRINTF1(buff2,"API %s\n",buff);
+#ifdef USE_MONITOR
+		A4GL_monitor_puts_int(buff2);
+#endif
+  }
+
+  if (strcmp ("ALL", acl_getenv ("DEBUG")) == 0
+      || strcmp (g_fname, acl_getenv ("DEBUG")) == 0)
+    {
+      va_start (args, fmt);
+	memset(buff,0,sizeof(buff));
+
+#ifdef  HAVE_VSNPRINTF
+	VSNPRINTF (buff, MAX_DEBUG,fmt, args);
+#else
+      	VSPRINTF (buff, fmt, args);
+#endif
+
+	buff[MAX_DEBUG]=0;
+      if (buff[strlen (buff) - 1] != ':')
+	FPRINTF (debugfile, "%-20s %-6d %-3s (%6ld,%6ld,%1d) %s%s",
+		 g_fname, g_lineno, g_level, a4gl_status, a4gl_sqlca.sqlcode,aclfgli_get_err_flg(),
+		 g_function, g_function[0] == 0 ? "": "() ");
+      else
+	FPRINTF (debugfile, "%-20s                       "," ");
+
+	if (a_strchr(buff,'\n')) 
+      		FPRINTF (debugfile, " %s", buff);
+	else
+      		FPRINTF (debugfile, " %s\n", buff);
+
+      fflush (debugfile);
+    }
+  indebug=0;
 }
 
 /**
@@ -215,6 +320,24 @@ A4GL_debug_full (char *fmt, ...)
   indebug=0;
 }
 
+
+/**
+ * 
+ * @todo Describe function
+ */
+int
+A4GL_set_line_extended (char *fname, long lineno, const char *level, const char *func)
+{
+  if (nodebug == DEBUG_NOTREQUIRED) { return 0; }
+  if (nodebug == 1)
+    {
+      strcpy (g_fname, fname);
+      strcpy (g_level, level);
+      strcpy (g_function, func);
+      g_lineno = lineno;
+    }
+  return 0;
+}
 
 /**
  * 
