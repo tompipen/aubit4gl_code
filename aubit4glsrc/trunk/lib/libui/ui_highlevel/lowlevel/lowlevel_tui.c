4abca8875cc9fd1a367bd63f4ec4ee81e32302e1
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2007-02-21 11:43:56 +0000

    .

--- aubit4glsrc/trunk/lib/libui/ui_highlevel/lowlevel/lowlevel_tui.c
+++ aubit4glsrc/trunk/lib/libui/ui_highlevel/lowlevel/lowlevel_tui.c
@@ -45,7 +45,7 @@ Assuming someone defined _XOPEN_SOURCE_EXTENDED...
 
 My curses.h is:
 
- $Id: lowlevel_tui.c,v 1.98 2007-02-21 10:37:10 mikeaubury Exp $ 
+ $Id: lowlevel_tui.c,v 1.99 2007-02-21 11:43:56 mikeaubury Exp $ 
  #define NCURSES_VERSION_MAJOR 5
  #define NCURSES_VERSION_MINOR 3 
  #define NCURSES_VERSION_PATCH 20030802
@@ -88,7 +88,7 @@ Looks like it was removed in Curses 5.3???!
 #include "formdriver.h"
 #ifndef lint
 static char const module_id[] =
-  "$Id: lowlevel_tui.c,v 1.98 2007-02-21 10:37:10 mikeaubury Exp $";
+  "$Id: lowlevel_tui.c,v 1.99 2007-02-21 11:43:56 mikeaubury Exp $";
 #endif
 int inprompt = 0;
 static void A4GL_local_mja_endwin (void);
@@ -3187,7 +3187,7 @@ A4GL_LL_clr_menu_disp (void *menu, int curr_width, int curr_height,
 
 
 void
-A4GL_LL_h_disp_title (void *menu, char *str, int curr_width, int curr_height,
+A4GL_LL_h_disp_title_with_attr (void *menu, char *str, int curr_width, int curr_height,
 		      int iscurrborder, int currwinno, void *cw, int gw_y,char *style, char*comment, char*image)
 {
   A4GL_wprintw ((void *) cw, 0, 1, gw_y, curr_width, curr_height,
