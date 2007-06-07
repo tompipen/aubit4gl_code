f30b0503973bc2248ce8c0cdd0c75c0b641fa17c
fortiz <fortiz@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2007-06-07 20:34:15 +0000

    CGI program generating report in format TXT, CSV, HTML and PDF see it at http://www.lacorona.com.mx/cgi-bin/html_report

---
+++ aubit4glsrc/trunk/tools/test/html/html_report.mk
@@ -0,0 +1,10 @@
+PROG            = html_report
+
+GLOBALS.4gl     = ahtmllib.4gl
+
+
+FILES.4gl       = \
+                        ${GLOBALS.4gl} \
+                        html_report.4gl
+
+FILES.per       =
