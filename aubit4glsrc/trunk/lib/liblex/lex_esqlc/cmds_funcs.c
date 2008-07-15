bf380f68c991286fafa9010b1d0145a404f41bd7
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2008-07-15 08:39:14 +0000

    Fix FINISH REPORT .. CONVERT TO

--- aubit4glsrc/trunk/lib/liblex/lex_esqlc/cmds_funcs.c
+++ aubit4glsrc/trunk/lib/liblex/lex_esqlc/cmds_funcs.c
@@ -3035,6 +3035,16 @@ int print_finish_cmd(struct_finish_cmd *cmd_data) {
   printc("} else {");
   print_copy_status_not_sql (0); /* Can have an ORDER BY which causes a series of SQL statements */
   printc("}");
+
+  if (cmd_data->conv_c) {
+		struct struct_convert_cmd cc;
+		cc.namespace=cmd_data->namespace;
+		cc.repname=cmd_data->repname;
+		cc.conv_c=cmd_data->conv_c;
+	 print_convert_cmd (&cc);
+	 printc("%s%s(3,REPORT_FREE);", cmd_data->namespace, cmd_data->repname);
+  }
+
   return 1;
 }
 
