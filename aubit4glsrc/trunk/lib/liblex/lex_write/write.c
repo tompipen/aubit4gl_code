4b6e72a7db69e2509f0f3934836842f9be51892f
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2008-09-10 11:25:22 +0000

    .

--- aubit4glsrc/trunk/lib/liblex/lex_write/write.c
+++ aubit4glsrc/trunk/lib/liblex/lex_write/write.c
@@ -1,11 +1,12 @@
 #include "a4gl_libaubit4gl.h"
+#include "a4gl_API_lex_lib.h"
 
 void LEXLIB_A4GL_initlex(void) {
 //
 }
 
 
-int LEXLIB_A4GL_write_generated_code(module_definition *module ){
+int LEXLIB_A4GL_write_generated_code(struct module_definition *module ){
 	char *outn="module.out";
 	
 	return A4GL_write_data_to_file("module_definition",module,module->module_name);
