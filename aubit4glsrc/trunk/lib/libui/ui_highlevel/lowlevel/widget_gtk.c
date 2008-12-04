9bde0a0ef5ef3086f876553c9445112cebdf75eb
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2008-12-04 20:13:30 +0000

    .

--- aubit4glsrc/trunk/lib/libui/ui_highlevel/lowlevel/widget_gtk.c
+++ aubit4glsrc/trunk/lib/libui/ui_highlevel/lowlevel/widget_gtk.c
@@ -1,6 +1,6 @@
 #ifndef lint
 static char const module_id[] =
-  "$Id: widget_gtk.c,v 1.42 2008-12-04 20:01:18 fortiz Exp $";
+  "$Id: widget_gtk.c,v 1.43 2008-12-04 20:13:30 mikeaubury Exp $";
 #endif
 #include <stdlib.h>
 #include "a4gl_libaubit4gl.h"
@@ -1916,7 +1916,7 @@ void A4GL_ll_display_blob(void *f) {
           	fwrite (b.ptr, b.memsize, 1, fwr);
           	fclose (fwr);
 	  } else {
-		A4GL_set_errm(filename);
+		A4GL_set_errm(fname);
 		A4GL_exitwith("Unable to open temporay file");
 		}
 	  filename=fname;
