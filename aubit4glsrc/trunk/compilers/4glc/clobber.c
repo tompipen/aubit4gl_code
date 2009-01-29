c9df2433c0de0758a0559fe841f4458603a85bc7
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2009-01-29 12:36:17 +0000

    .

--- aubit4glsrc/trunk/compilers/4glc/clobber.c
+++ aubit4glsrc/trunk/compilers/4glc/clobber.c
@@ -24,7 +24,7 @@
 # | contact licensing@aubit.com                                           |
 # +----------------------------------------------------------------------+
 #
-# $Id: clobber.c,v 1.6 2008-11-12 14:57:07 mikeaubury Exp $
+# $Id: clobber.c,v 1.7 2009-01-29 12:36:17 mikeaubury Exp $
 #
 */
 
@@ -134,8 +134,8 @@ add_clobber (module_definition *mod, char *buff_orig, char *important)
 
 
 
-  if (strlen (buff_orig) <= 20)
-    {				/* Extra 2 for the quotes... */
+  if (strlen (buff_orig) <= 18)
+    {			
       mod->clobberings.clobberings_val[mod->clobberings.clobberings_len - 1].origval = acl_strdup (buff_orig);
       mod->clobberings.clobberings_val[mod->clobberings.clobberings_len - 1].newval = acl_strdup (buff_new);
       mod->clobberings.clobberings_val[mod->clobberings.clobberings_len - 1].important = acl_strdup (important);
