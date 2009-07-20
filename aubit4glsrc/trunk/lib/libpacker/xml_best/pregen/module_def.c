ee95f02de6ce190a633c4099fb82180875304fae
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2009-07-20 14:13:44 +0000

    Fixup for MID 1317 (build test 1807)

--- aubit4glsrc/trunk/lib/libpacker/xml_best/pregen/module_def.c
+++ aubit4glsrc/trunk/lib/libpacker/xml_best/pregen/module_def.c
@@ -1,6 +1,6 @@
 /* XML application for module_def.dtd.
  * Includes actions from module_def.act.
- * Generated 2009/07/17 17:29:23.
+ * Generated 2009/07/20 14:34:38.
  *
  * This program was generated with the FleXML XML processor generator.
  * FleXML is Copyright (C) 1999-2005 Kristoffer Rose.  All rights reserved.
