b67f654bc357a89c50b9fdb4f3d1c47a3799e739
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2009-10-12 18:19:40 +0000

    .

--- aubit4glsrc/trunk/lib/libui/ui_xml/uilib/xml/pregenerated/triggers.c
+++ aubit4glsrc/trunk/lib/libui/ui_xml/uilib/xml/pregenerated/triggers.c
@@ -9493,7 +9493,7 @@ int xml_yy_flex_debug = 0;
 char *xml_yytext;
 #line 1 "triggers.l"
 /* Validating XML processor for triggers.dtd.
- * Generated 2009/10/10 12:08:07.
+ * Generated 2009/10/12 19:19:15.
  *
  * This program was generated with the FleXML XML processor generator.
  * FleXML is Copyright (C) 1999-2005 Kristoffer Rose.  All rights reserved.
--- aubit4glsrc/trunk/lib/libui/ui_xml/uilib/xml/pregenerated/triggers_act.c
+++ aubit4glsrc/trunk/lib/libui/ui_xml/uilib/xml/pregenerated/triggers_act.c
@@ -1,6 +1,6 @@
 /* XML application for triggers.dtd.
  * Includes actions from triggers_act.act.
- * Generated 2009/10/10 12:08:07.
+ * Generated 2009/10/12 19:19:15.
  *
  * This program was generated with the FleXML XML processor generator.
  * FleXML is Copyright (C) 1999-2005 Kristoffer Rose.  All rights reserved.
