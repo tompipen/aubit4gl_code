4d89b4cb9fb5dfc412eba22d0db5c0d28b91fd72
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2010-01-14 15:18:08 +0000

    .

---
+++ aubit4glsrc/trunk/tools/a4gl/web.sql
@@ -0,0 +1,34 @@
+create table web_application 
+  (
+    application char(18),
+    connmode char(1),
+    pxy_programname char(18),
+    pxy_server char(32),
+    pxy_port char(10),
+    pxy_username char(18),
+    pxy_password char(18),
+    auth_cmdline char(256)
+  );
+
+create table web_user 
+  (
+    username char(18),
+    password char(48)
+  );
+
+create table web_perms 
+  (
+    url_id char(18),
+    application char(18),
+    username char(18)
+  );
+
+create table web_serverurl 
+  (
+    url_id char(18),
+    relativeurl char(64),
+    authmode char(1),
+    htaccessfile char(64),
+    htpasswdfile char(64),
+    xmlfile char(64)
+  );
