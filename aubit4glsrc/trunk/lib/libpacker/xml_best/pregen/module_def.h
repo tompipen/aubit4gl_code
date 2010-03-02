17b19768c16444de65e06fc8fe8a8ae7b6197a8d
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2010-03-02 15:31:32 +0000

    .

--- aubit4glsrc/trunk/lib/libpacker/xml_best/pregen/module_def.h
+++ aubit4glsrc/trunk/lib/libpacker/xml_best/pregen/module_def.h
@@ -1,5 +1,5 @@
 /* XML processor/application API for module_def.dtd.
- * Generated 2010/02/28 09:32:36.
+ * Generated 2010/03/01 12:07:02.
  *
  * This program was generated with the FleXML XML processor generator.
  * FleXML is Copyright (C) 1999-2005 Kristoffer Rose.  All rights reserved.
