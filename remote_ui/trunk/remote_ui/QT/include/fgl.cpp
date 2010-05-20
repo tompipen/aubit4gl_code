b4b987e3f35a9edc7e231b511356cbac019ff127
dbuettner <dbuettner@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2010-05-20 13:25:53 +0000

    FORMAT

--- remote_ui/trunk/remote_ui/QT/include/fgl.cpp
+++ remote_ui/trunk/remote_ui/QT/include/fgl.cpp
@@ -5,14 +5,17 @@
 namespace Fgl {
    QHash<QString, QString> env;
 
-   bool usingFunc(QString fmt, QString *value, DataType dataType)
+   QString usingFunc(QString fmt, QString value, DataType dataType)
    {
+      if(value.isEmpty())
+         return value;
+
       switch(dataType){
          case DTYPE_DECIMAL:
          case DTYPE_MONEY:
             {
             bool isneg = false;
-            QByteArray p = value->toAscii();
+            QByteArray p = value.toAscii();
             for (int a = 0; a < p.size(); a++)
             {
                if (p[a] >= '0' && p[a] <= '9') continue;
@@ -21,7 +24,7 @@ namespace Fgl {
                p[a] = ' ';
             }
 
-            fgl_using_string(fmt, value, isneg);
+            return fgl_using_string(fmt, value, isneg);
 
             
             }
@@ -32,11 +35,11 @@ namespace Fgl {
          case DTYPE_SERIAL:
          case DTYPE_SMFLOAT:
          case DTYPE_FLOAT:
-            fgl_using(fmt, value);
+            return fgl_using(fmt, value);
             break;
 
          case DTYPE_DATE:
-            fgl_using_date(fmt, value);
+            return fgl_using_date(fmt, value);
             break;
          case DTYPE_CHAR:
          case DTYPE_NULL:
@@ -45,7 +48,6 @@ namespace Fgl {
          case DTYPE_VCHAR:
          case DTYPE_INTERVAL:
          case DTYPE_NCHAR:
-            break;
          default:
             if (fmt.contains("dd") || fmt.contains("mm") || fmt.contains("yy") ||
                 fmt.contains("DD") || fmt.contains("MM") || fmt.contains("YY"))
@@ -57,34 +59,496 @@ namespace Fgl {
                 return fgl_using(fmt, value);
             }
       }
-      return true;
+      return value;
    }
 
-   bool fgl_using(QString fmt, QString *value)
+   QString fgl_using(QString fmt, QString value)
    {
       Q_UNUSED(fmt);
       Q_UNUSED(value);
-      return true;
+
+      bool isNeg = false;
+      bool ok = false;
+      qreal dec = value.toDouble(&ok);
+
+      if(ok){
+         if(dec < 0)
+            isNeg = true;
+      }
+
+      return fgl_using_string(fmt, value, isNeg);
    }
 
-   bool fgl_using_date(QString fmt, QString *value)
+   QString fgl_using_date(QString fmt, QString value)
    {
-      QDate d = getDate(*value);
+      QDate d = getDate(value);
       fmt = fmt.replace("mmmm", "MMMM");
       fmt = fmt.replace("mmm", "MMM");
       fmt = fmt.replace("mm", "MM");
-      *value = d.toString(fmt);
+      value = d.toString(fmt);
 
-      return true;
+      return value;
    }
 
-   bool fgl_using_string(QString fmt, QString *value, bool isneg)
+   QString fgl_using_string(QString fmt, QString value, bool isneg)
    {
-       Q_UNUSED(fmt);
-       Q_UNUSED(value);
-       Q_UNUSED(isneg);
-      return true;
+      Q_UNUSED(fmt);
+      Q_UNUSED(value);
+      Q_UNUSED(isneg);
+
+      QString overflow;
+      bool hasMoney = false;
+      QList<QChar> canFloatHead;
+      canFloatHead << '*' << '-' << '+' << '(' << '$';
+      QList<QChar> repDigit;
+      repDigit << '*' << '&' << '#' << '<' << '-' << '+' << '(' << ')' << '$';
+      int def = 30;
+
+      int lb, cb = 0;
+
+      int s;
+
+      s = fmt.length();
+
+      overflow.fill('*', s);
+
+
+      for(int a=0; a<s; a++){
+         if(fmt.at(a) == '(')
+            lb++;
+         if(fmt.at(a) == ')')
+            cb++;
+      }
+
+      if(lb > 1 && cb > 1){
+         //TODO: ERROR
+      }
+
+      if(lb > 0 && cb > 0){
+         QString buff = fmt.mid(fmt.indexOf("(")+1);
+         int eob = buff.indexOf(")");
+         if(eob == -1){
+            return QString("");
+         }
+         buff = buff.mid(0, eob-1);
+         for(int a=0; a<buff.length(); a++){
+            if(lb == 1 && cb == 1){
+               if(buff.at(a) == '(')
+                  buff[a] = '#';
+               if(buff.at(a) == ')')
+                  buff[a] = '#';
+            }
+
+            if(lb > 1 && cb == 1){
+               if(buff.at(a) == '(')
+                  buff[a] = '#';
+            }
+
+            if(lb == 1 && cb > 1){
+               if(buff.at(a) == ')')
+                  buff[a] = '#';
+            }
+         }
+
+         QString res = fgl_using_string(buff, value, false);
+         QString fmtOrig = fmt;
+         fmt = fmt.mid(0, fmt.indexOf('(')+1);
+         fmt += res;
+         fmt += fmtOrig.mid(eob+1);
+
+         if(!isneg){
+            if(fmt.indexOf('(') >= 0 && fmt.indexOf(')') >= 0){
+               buff = fmt.replace('(', ' ');
+               fmt = fmt.replace(')', ' ');
+            }
+            else{
+               if(lb > 1){
+                  for(int a=0; a<fmt.length()-1; a++){
+                     if(fmt.at(a) == '(' && fmt.at(a+1) == ' '){
+                        fmt[a] = ' ';
+                        fmt[a+1] = '(';
+                     }
+                  }
+               }
+
+               if(cb > 1){
+                  for(int a=0; a<fmt.length()-1; a++){
+                     if(fmt.at(a) == ' ' && fmt.at(a+1) == ')'){
+                        fmt[a] = ')';
+                        fmt[a+1] = ' ';
+                     }
+                  }
+               }
+            }
+         }
+
+         return fmt;
+      }
+
+      bool ok = false;
+
+      value.toDouble(&ok);
+
+      if(!ok)
+         return "";
+
+      int num_places = 0;
+      QString fm1;
+      QString fm2;
+      QString lbuff;
+
+      if (fmt.indexOf('.') >= 0){
+         QString lbuff;
+         int pos_dot;
+         pos_dot = fmt.indexOf('.');
+         lbuff = fmt.mid(pos_dot + 1);
+         fm1 = fmt.mid(0, pos_dot);
+         lbuff.trimmed();
+         num_places = lbuff.length();
+         fm2 = lbuff;
+      }
+      else
+      {
+         fm1 = fmt;
+         fm2 = "";
+         num_places = 0;
+      }
+
+      //TODO: find a better way
+      QString qs_buffInteger;
+      QString qs_buffDecimal;
+
+      bool decFound = false;
+      for(int i=0; i<value.length(); i++){
+         if(value.at(i) == '.'){
+            decFound = true;
+            continue;
+         }
+
+         if(!decFound){
+            qs_buffInteger += value.at(i);
+         }
+         else{
+            qs_buffDecimal += value.at(i);
+         }
+      }
+
+      qs_buffInteger = qs_buffInteger.rightJustified(31);
+
+      if(num_places > 64 || qs_buffDecimal.length() >= 64){
+         return overflow;
+      }
+
+      qs_buffDecimal.append("000000000000000000000000000000000");
+      qs_buffDecimal.mid(0,32);
+
+      QString qs_str = fmt;
+      bool isprnt = true;
+
+      int f_cnt = 0;
+      int d_cnt = 0;
+      int n_cnt = 0;
+
+      if(isneg){
+         n_cnt = 1;
+      }
+      else{
+         n_cnt = 0;
+      }
+
+      if(isneg){
+         if(fmt.contains('(') || fmt.contains(')') || fmt.contains('+') || fmt.contains('-')){
+         }
+         else{
+            n_cnt = 0;
+         }
+      }
+      else{
+         if(fmt.contains('(') || fmt.contains(')') || fmt.contains('+') || fmt.contains('-')){
+            n_cnt = 1;
+         }
+      }
+
+      decFound = false;
+      for(int i=0; i<fmt.length(); i++){
+         if(fmt.at(i) == '.'){
+            decFound = true;
+            continue;
+         }
+
+         if(!decFound){
+            if(repDigit.contains(fmt.at(i)))
+               f_cnt++;
+         }
+         else{
+            if(repDigit.contains(fmt.at(i)))
+               d_cnt++;
+         }
+      }
+
+      for (int a = def; (a > 0 && qs_buffInteger.at(a) != ' '); a--)
+         n_cnt++;
+
+
+      if(fmt.contains('$')){
+         hasMoney = true;
+      }
+      else{
+         hasMoney = false;
+      }
+
+      if(f_cnt < n_cnt + (hasMoney?1:0)){
+         int a = fmt.length();
+         if(a > s){
+            a = s;
+         }
+
+         if(n_cnt > a)
+         {
+            return overflow;
+         }
+
+         return overflow;
+      }
+
+
+      for(int a = fm1.length()-1; a >= 0; a--){
+         if(repDigit.contains(fm1.at(a))){
+            if(def >= 0){
+               if(((qs_buffInteger.at(def) == '0' && qs_buffInteger.at(def) == ' ') || qs_buffInteger[def] == ' ' ) && isprnt)
+               {
+                  isprnt = false;
+               }
+            }
+            qs_str[a] = qs_buffInteger.at(def--);
+            if(!isprnt){
+               if(fm1.at(a) == '#'){
+                  qs_str[a] = ' ';
+                  continue;
+               }
+
+               if(fm1.at(a) == '*'){
+                  qs_str[a] = '*';
+                  continue;
+               }
+
+               if(fm1.at(a) == '&'){
+                  qs_str[a] = '0';
+                  continue;
+               }
+
+               if(fm1.at(a) == '<'){
+                  qs_str[a] = '<';
+                  continue;
+               }
+
+               int p = canFloatHead.indexOf(fm1.at(a));
+               if(p >= 0){
+                  //canFloatHead[p] = QChar(1);
+                  if(fm1.at(a) == '+' && isneg){
+                     qs_str[a] = '-';
+                     continue;
+                  }
+
+                  if(fm1.at(a) == '+' && !isneg){
+                     qs_str[a] = '+';
+                     continue;
+                  }
+
+                  if(fm1.at(a) == '-' && isneg){
+                     qs_str[a] = '-';
+                     continue;
+                  }
+
+                  if(fm1.at(a) == '-' && !isneg){
+                     qs_str[a] = ' ';
+                     continue;
+                  }
+
+                  if(fm1.at(a) == '('){
+                     if(isneg){
+                        qs_str[a] = '(';
+                        continue;
+                     }
+                     else{
+                        qs_str[a] = ' ';
+                        continue;
+                     }
+                  }
+                  if(fm1.at(a) == ')' && isneg){
+                     qs_str[a] = ')';
+                     continue;
+                  }
+                  qs_str[a] = fm1.at(a);
+                  continue;
+               }
+               else{
+                  qs_str[a] = ' ';
+                  continue;
+               }
+            }
+         }
+         else{
+            if(qs_buffInteger.at(def) == ' ' && qs_str.at(a) == ','){
+               if(fm1.at(a+1) == '<'){
+                  qs_str[a] = '<';
+               }
+               else{
+                  qs_str[a] = QChar((char)0x01);
+               }
+            }
+         }
+      }
+      def = 0;
+
+      for(int a = 0; a < fm2.length(); a++){
+         if(repDigit.contains(fm2.at(a))){
+            if(fm2.at(a) == ')'){
+               if(isneg){
+                  qs_str[a+fm1.length()+1] = ')';
+               }
+               else{
+                  qs_str[a+fm1.length()+1] = ' ';
+               }
+            }
+            qs_str[a+fm1.length()+1] = qs_buffDecimal.at(def++);
+         }
+         else{
+            qs_str[a+fm1.length()+1] = fm2.at(a);
+         }
+      }
+
+      if(qs_str.contains("<")){
+         QString buffx;
+         def = 0;
+         for(int a =0; a < qs_str.length(); a++){
+            if(qs_str.at(a) == '<'){
+               if(qs_str.at(a+1) == ','){
+                  qs_str[a+1] = '<';
+               }
+               continue;
+            }
+            if(!isneg && qs_str.at(a) == '-' && qs_str.at(a+1) == '<'){
+               continue;
+            }
+            buffx += qs_str.at(a);
+         }
+
+         qs_str = buffx;
+      }
+
+      for(int a=0; a < qs_str.length(); a++){
+         if(qs_str.at(a) == QChar((char)0x01)){
+            if(a == 0){
+               qs_str[a] = ' ';
+            }
+            else{
+               qs_str[a] = qs_str.at(a-1);
+            }
+            if(a == 1){
+               if(qs_str.at(a-1) == '$'){
+                   qs_str[a-1] = ' ';
+               }
+               if(qs_str.at(a-1) == '-'){
+                   qs_str[a-1] = ' ';
+               }
+               if(qs_str.at(a-1) == '('){
+                   qs_str[a-1] = ' ';
+               }
+            }
+            if(a >= 2){
+               if(qs_str.at(a-1) == '$' && qs_str.at(a-2) != '$'){
+                   qs_str[a-1] = ' ';
+               }
+               if(qs_str.at(a-1) == '-' && qs_str.at(a-2) != '-'){
+                   qs_str[a-1] = ' ';
+               }
+               if(qs_str.at(a-1) == '(' && qs_str.at(a-2) != '('){
+                   qs_str[a-1] = ' ';
+               }
+            }
+         }
+      }
+
+      QString prefix;
+      QChar dec;
+      QString suffix;
+      const QString DBMONEY = env["DBMONEY"];
+      decFound = false;
+      for(int i=0;i<DBMONEY.size(); i++){
+         if(DBMONEY.at(i) == QChar('.') || DBMONEY.at(i) == QChar(',')){
+            decFound = true;
+            dec = DBMONEY.at(i);
+            continue;
+         }
+
+         if(!decFound){
+            prefix += DBMONEY.at(i);
+         }
+         else{
+            suffix += DBMONEY.at(i);
+         }
+      }
+
+      for(int a=qs_str.length()-1; a>=0; a--){
+         if(qs_str.at(a) == '.'){
+            qs_str[a] = dec;
+         }
+      }
+
+      if(hasMoney && !qs_str.contains('$')){
+         int firstNonSpace = -1;
+         for(int a = 0; a>qs_str.length(); a++){
+            if(qs_str.at(a) != ' '){
+               firstNonSpace = a;
+            }
+         }
+         if(firstNonSpace > 0){
+            if(qs_str.at(firstNonSpace) == '-' || qs_str.at(firstNonSpace) == '(' || qs_str.at(firstNonSpace) == '+'){
+               qs_str[firstNonSpace-1] = qs_str.at(firstNonSpace);
+               qs_str[firstNonSpace] = '$';
+            }
+            else{
+               qs_str[firstNonSpace-1] = '$';
+            }
+         }
+      }
+      
+      return qs_str;
    }
+/*
+      QString prefix;
+      QChar dec;
+      QString suffix;
+      const QString DBMONEY = env["DBMONEY"];
+      bool decFound = false;
+      QString new_value = value;
+
+      for(int i=0;i<DBMONEY.size(); i++){
+         if(DBMONEY.at(i) == QChar('.') || DBMONEY.at(i) == QChar(',')){
+            decFound = true;
+            dec = DBMONEY.at(i);
+            continue;
+         }
+
+         if(!decFound){
+            prefix += DBMONEY.at(i);
+         }
+         else{
+            suffix += DBMONEY.at(i);
+         }
+      }
+
+      for(int i=0; i<new_value.size(); i++){
+          if(new_value.at(i) == QChar('.'))
+             new_value.replace(i, dec);
+      }
+
+      value = new_value;
+
+      return value;
+   }
+*/
 
    DataType decodeDataType(QString type)
    {
