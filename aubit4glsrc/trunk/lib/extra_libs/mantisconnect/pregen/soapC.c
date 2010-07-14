3bd22ccbf8fc38385f25b12c7eb7bc47a883c9ff
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2010-07-14 13:43:34 +0000

    .

--- aubit4glsrc/trunk/lib/extra_libs/mantisconnect/pregen/soapC.c
+++ aubit4glsrc/trunk/lib/extra_libs/mantisconnect/pregen/soapC.c
@@ -16,7 +16,7 @@
 extern "C" {
 #endif
 
-SOAP_SOURCE_STAMP("@(#) soapC.c ver 2.7.17 2010-07-14 12:56:59 GMT")
+SOAP_SOURCE_STAMP("@(#) soapC.c ver 2.7.17 2010-07-14 13:40:32 GMT")
 
 
 #ifndef WITH_NOGLOBAL
