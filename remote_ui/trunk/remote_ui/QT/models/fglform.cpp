3d40719d9ee4b98f474a8bca1411da556eba28ca
dbuettner <dbuettner@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2010-10-05 14:04:08 +0000

    corrected one line

--- remote_ui/trunk/remote_ui/QT/models/fglform.cpp
+++ remote_ui/trunk/remote_ui/QT/models/fglform.cpp
@@ -982,7 +982,7 @@ void FglForm::setFormLayout(const QDomDocument& docLayout)
          connect(lineEdit, SIGNAL(customContextMenuRequested(const QPoint&)), this, SLOT(createContextMenu(const QPoint&)));
          connect(lineEdit, SIGNAL(error(const QString&)), this, SLOT(error(const QString&)));
          connect(lineEdit, SIGNAL(textEdited(QString)), this, SLOT(setBufferTouched()));
-         connect(lineEdit, SIGNAL(cursorPositionChanged(int)), this, SLOT(setLastCursor(int, int)));
+         connect(lineEdit, SIGNAL(cursorPositionChanged(int, int)), this, SLOT(setLastCursor(int, int)));
          lineEdit->installEventFilter(this);
       }
 
