24b5c655fbdce3352edff206cbe92fd2b406801d
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2011-02-28 18:07:54 +0000

    .

--- aubit4glsrc/trunk/lib/libui/ui_curses/generic_ui.c
+++ aubit4glsrc/trunk/lib/libui/ui_curses/generic_ui.c
@@ -1,7 +1,7 @@
 #include "a4gl_lib_ui_tui_int.h"
 #ifndef lint
 	static char const module_id[] =
-		"$Id: generic_ui.c,v 1.60 2010-12-11 13:08:56 mikeaubury Exp $";
+		"$Id: generic_ui.c,v 1.61 2011-02-28 18:07:54 mikeaubury Exp $";
 #endif
 
 static int A4GL_find_shown (ACL_Menu * menu, int chk, int dir);
@@ -865,7 +865,13 @@ A4GL_size_menu (ACL_Menu * menu)
 
 	  something_else_printable=0;
 	  s_off=0;
-          if (disp_cnt2 + menu->menu_offset + s_length  + 4 > menu->w) { // was 5
+
+	  int menu_width_chk=5;
+	  if (A4GL_isyes(acl_getenv("MENUOFFSET4"))) {
+	   	menu_width_chk=4;
+	  }
+
+          if ((disp_cnt2 + menu->menu_offset + s_length  + menu_width_chk) > menu->w) { 
   		ACL_Menu_Opts *po;
       		po = (ACL_Menu_Opts *) opt1->next_option;
 		while(po!=0) {
