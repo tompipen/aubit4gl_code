bf4d243ca9967eb4a84903c687de1bc098ea6c36
mseidel <mseidel@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2011-07-21 20:06:59 +0000

    umlauts

--- remote_ui/trunk/remote_ui/QT/clienttcp.cpp
+++ remote_ui/trunk/remote_ui/QT/clienttcp.cpp
@@ -2271,28 +2271,28 @@ MainFrame::vdcdebug("ProtocolHandler","saveFile", "const QDomNode &domNode, QStr
 QString ProtocolHandler::filterUmlauts(QString qs_text)
 {
 MainFrame::vdcdebug("ProtocolHandler","filterUmlauts", "QString qs_text");
-   qs_text.replace(QChar(129), QString::fromUtf8("Ã¼")); 
-   qs_text.replace(QChar(130), QString::fromUtf8("Ã©")); 
-   qs_text.replace(QChar(131), QString::fromUtf8("Ã¢")); 
-   qs_text.replace(QChar(132), QString::fromUtf8("Ã¤")); 
-   qs_text.replace(QChar(133), QString::fromUtf8("Ã ")); 
-   qs_text.replace(QChar(134), QString::fromUtf8("Ã¥")); 
-   qs_text.replace(QChar(135), QString::fromUtf8("Ã§")); 
-   qs_text.replace(QChar(136), QString::fromUtf8("Ãª")); 
-   qs_text.replace(QChar(137), QString::fromUtf8("Ã«")); 
-   qs_text.replace(QChar(138), QString::fromUtf8("Ã¨")); 
-   qs_text.replace(QChar(139), QString::fromUtf8("Ã¯")); 
-   qs_text.replace(QChar(140), QString::fromUtf8("Ã®")); 
-   qs_text.replace(QChar(141), QString::fromUtf8("Ã¬")); 
-   qs_text.replace(QChar(145), QString::fromUtf8("Ã¦")); 
-   qs_text.replace(QChar(146), QString::fromUtf8("Ã†")); 
-   qs_text.replace(QChar(147), QString::fromUtf8("Ã´")); 
-   qs_text.replace(QChar(148), QString::fromUtf8("Ã¶")); 
-   qs_text.replace(QChar(149), QString::fromUtf8("Ã²")); 
-   qs_text.replace(QChar(150), QString::fromUtf8("Ã»")); 
-   qs_text.replace(QChar(225), QString::fromUtf8("ÃŸ")); 
+qs_text.replace(QChar(129), QString::fromUtf8("ü"));
+qs_text.replace(QChar(130), QString::fromUtf8("é"));
+qs_text.replace(QChar(131), QString::fromUtf8("â"));
+qs_text.replace(QChar(132), QString::fromUtf8("ä"));
+qs_text.replace(QChar(133), QString::fromUtf8("à"));
+qs_text.replace(QChar(134), QString::fromUtf8("å"));
+qs_text.replace(QChar(135), QString::fromUtf8("ç"));
+qs_text.replace(QChar(136), QString::fromUtf8("ê"));
+qs_text.replace(QChar(137), QString::fromUtf8("ë"));
+qs_text.replace(QChar(138), QString::fromUtf8("è"));
+qs_text.replace(QChar(139), QString::fromUtf8("ï"));
+qs_text.replace(QChar(140), QString::fromUtf8("î"));
+qs_text.replace(QChar(141), QString::fromUtf8("ì"));
+qs_text.replace(QChar(145), QString::fromUtf8("æ"));
+qs_text.replace(QChar(146), QString::fromUtf8("Æ"));
+qs_text.replace(QChar(147), QString::fromUtf8("ô"));
+qs_text.replace(QChar(148), QString::fromUtf8("ö"));
+qs_text.replace(QChar(149), QString::fromUtf8("ò"));
+qs_text.replace(QChar(150), QString::fromUtf8("û"));
+qs_text.replace(QChar(225), QString::fromUtf8("ß"));
 
-   qs_text.replace(QChar(245), QString::fromUtf8("Ã¤")); 
+qs_text.replace(QChar(245), QString::fromUtf8("ä"));
 
    return qs_text;
 }
