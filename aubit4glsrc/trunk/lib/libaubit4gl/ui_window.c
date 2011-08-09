ba045d86142462eb38af0e75ae51f80a8365cbb1
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2011-08-09 16:20:54 +0000

    .

--- aubit4glsrc/trunk/lib/libaubit4gl/ui_window.c
+++ aubit4glsrc/trunk/lib/libaubit4gl/ui_window.c
@@ -133,12 +133,12 @@ static int ui_form_setelementhidden(long *objectID, int nParam) {
 static int ui_form_setfieldhidden(long *objectID, int nParam) {
         struct ui_form_data *data;
         struct sObject *ptr;
-	int vis;
+	short vis;
 	char *field;
         struct BINDING ibind[]= {
                 {NULL,0,32,0,0,0},
                 {NULL,0,32,0,0,0},
-                {NULL,2,0,0,0,0}
+                {NULL,1,0,0,0,0}
         };
 
 
