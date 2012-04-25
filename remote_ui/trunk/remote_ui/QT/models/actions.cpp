053cc3531bb0f631d4aa243ac25630504adfc175
dkroker <dkroker@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2012-04-25 14:21:09 +0000

    fixed esc button

--- remote_ui/trunk/remote_ui/QT/models/actions.cpp
+++ remote_ui/trunk/remote_ui/QT/models/actions.cpp
@@ -130,7 +130,8 @@ MainFrame::vdcdebug("Actions","parseElement", "QDomNode xmlNode");
           } else {
               Action *action = new Action(text.toLower(), text, p_fglform);
               qsl_names << text.toLower();
-              action->setAcceleratorName2(shortcut);
+              action->setAcceleratorName(shortcut);
+              action->setShortcut(shortcut);
               ql_actions << action;
           }
       }
