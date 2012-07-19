2335d6970a089bf795ad2cc673391a03dcdd7023
mseidel <mseidel@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2012-07-19 14:18:13 +0000

    MESSAGE AND ERROR is now displayed in the active fglform if the programm have no own window

--- remote_ui/trunk/remote_ui/QT/screenhandler.cpp
+++ remote_ui/trunk/remote_ui/QT/screenhandler.cpp
@@ -2536,6 +2536,12 @@ MainFrame::vdcdebug("ScreenHandler","displayError", "QString text");
 
       if(text.isEmpty())
         return;
+      if(FglForm *form = qobject_cast<FglForm*> (QApplication::activeWindow()))
+      {
+         StatusBar *sb = (StatusBar*) form->statusBar();
+         sb->displayError(text);
+         return;
+      }
       Dialog *errorDialog = new Dialog(tr("Error"), text, "dialog", "stop", NULL);
       errorDialog->setModal(true);
       errorDialog->createButton(1, "OK", "OK", "ok.png");
@@ -2574,7 +2580,13 @@ MainFrame::vdcdebug("ScreenHandler","displayMessage", "QString text");
    {
        if(text.isEmpty())
          return;
-      Dialog *errorDialog = new Dialog(tr("Message"), text, "dialog", "info", NULL);
+       if(FglForm *form = qobject_cast<FglForm*> (QApplication::activeWindow()))
+       {
+          StatusBar *sb = (StatusBar*) form->statusBar();
+          sb->displayError(text);
+          return;
+       }
+       Dialog *errorDialog = new Dialog(tr("Message"), text, "dialog", "info", NULL);
       errorDialog->setModal(true);
       errorDialog->createButton(1, "OK", "OK", "ok.png");
       QPushButton *qpb = (QPushButton *) errorDialog->getAction("OK")->parent();
