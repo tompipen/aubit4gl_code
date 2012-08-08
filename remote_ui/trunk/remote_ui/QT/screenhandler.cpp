c0b4c3b442f7806d4844f543da8f2f308d709130
dkroker <dkroker@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2012-08-08 14:26:34 +0000

    FIX: A Critical bug in the last Checkin

--- remote_ui/trunk/remote_ui/QT/screenhandler.cpp
+++ remote_ui/trunk/remote_ui/QT/screenhandler.cpp
@@ -3336,7 +3336,7 @@ void ScreenHandler::activeFocus()
     if(p_fglform->dialog() == NULL && p_fglform->ringMenuPulldown() == NULL){
         p_fglform->setWindowState(Qt::WindowActive);
         p_fglform->raise();
-        QApplication::setActiveWindow((QWidget*) p_fglform->ringMenuPulldown());
+        QApplication::setActiveWindow((QWidget*) p_fglform);
         p_fglform->activateWindow();
         //Qt3 weg, static QMetaobject::invokeMethod in zukunft
         QApplication::postEvent(this, new QEvent((QEvent::Type)1337));
@@ -3345,6 +3345,12 @@ void ScreenHandler::activeFocus()
     if(p_fglform->dialog() != NULL)
     {
         QApplication::setActiveWindow((QWidget*) p_fglform->dialog());
+        p_fglform->dialog()->raise();
+        p_fglform->dialog()->activateWindow();
+    }
+    if(p_fglform->ringMenuPulldown() != NULL)
+    {
+        QApplication::setActiveWindow((QWidget*) p_fglform->ringMenuPulldown());
         p_fglform->dialog()->raise();
         p_fglform->dialog()->activateWindow();
     }
