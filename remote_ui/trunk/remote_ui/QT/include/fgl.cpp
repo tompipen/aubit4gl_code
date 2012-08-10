9d550ffb2ece411f055416fff946907d7d5a1593
dkroker <dkroker@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2012-08-10 07:41:04 +0000

    FIX: A Problem with TextEdit Field and AutoNext

--- remote_ui/trunk/remote_ui/QT/include/fgl.cpp
+++ remote_ui/trunk/remote_ui/QT/include/fgl.cpp
@@ -65,6 +65,7 @@ namespace Fgl {
          case DTYPE_INTERVAL:
           if(picture.contains("##:") && value.trimmed().indexOf(":") == 1)
              value = value.trimmed().prepend("0");
+             qDebug() << "value: " << value;
              break;
          case DTYPE_NCHAR:
          default:
