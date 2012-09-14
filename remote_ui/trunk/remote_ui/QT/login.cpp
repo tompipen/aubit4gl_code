40d7be58ed2239bb46d8e4bfb99d641236899147
dkroker <dkroker@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2012-09-14 11:08:09 +0000

    FIX: missing slash

--- remote_ui/trunk/remote_ui/QT/login.cpp
+++ remote_ui/trunk/remote_ui/QT/login.cpp
@@ -325,7 +325,7 @@ void LoginForm::aboutVDC(QWidget *parent)
     QList<QList<QString> > returnList;
 
     #ifdef Q_WS_MAC
-        file.setFileName(QDir::currentPath() + "VDC.app/Contents/MacOS/versions.xml");
+        file.setFileName(QDir::currentPath() + "/VDC.app/Contents/MacOS/versions.xml");
     #else
         file.setFileName(QApplication::applicationDirPath() + "/versions.xml");
     #endif
