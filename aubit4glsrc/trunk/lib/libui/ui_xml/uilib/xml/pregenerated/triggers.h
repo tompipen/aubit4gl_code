4695c9e551fe758d29f9eb548bc373fabe8c4188
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2014-01-05 12:29:10 +0000

    .

--- aubit4glsrc/trunk/lib/libui/ui_xml/uilib/xml/pregenerated/triggers.h
+++ aubit4glsrc/trunk/lib/libui/ui_xml/uilib/xml/pregenerated/triggers.h
@@ -1,5 +1,5 @@
 /* XML processor/application API for triggers.dtd.
- * Generated 2014/01/05 11:34:24.
+ * Generated 2014/01/05 12:28:24.
  *
  * This program was generated with the FleXML XML processor generator.
  * FleXML is Copyright (C) 1999-2005 Kristoffer Rose.  All rights reserved.
