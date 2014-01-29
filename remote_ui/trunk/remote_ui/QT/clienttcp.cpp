82569cbb300f7dc154a7443c6037cfae95535842
dkroker <dkroker@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2014-01-29 16:39:41 +0000

    removed umlauts filter.

--- remote_ui/trunk/remote_ui/QT/clienttcp.cpp
+++ remote_ui/trunk/remote_ui/QT/clienttcp.cpp
@@ -1174,7 +1174,7 @@ if(childElement.nodeName() == "PROGRAMSTARTUP"){
          in_request << xmlFileString;
          xmlFileString = in_request.readAll();
 */
-         xmlFileString = filterUmlauts(xmlFileString);
+         xmlFileString = xmlFileString;
          QMetaObject::invokeMethod(p_currScreenHandler, "handleXMLStartMenu", Qt::QueuedConnection, Q_ARG(QString, xmlFileString));
          //handleXMLStartMenu(xmlFileString);
       return;
@@ -3037,7 +3037,7 @@ MainFrame::vdcdebug("ProtocolHandler","handleDisplayToElement", "const QDomNode&
                    QByteArray ba;
                    ba.append(textElement.text());
                    QByteArray ba2 = QByteArray::fromBase64(ba);
-                   qs_text = filterUmlauts(QString(ba2));
+                   qs_text = QString(ba2);
                }
 
 
@@ -3761,40 +3761,6 @@ MainFrame::vdcdebug("ProtocolHandler","saveFile", "const QDomNode &domNode, QStr
 
 }
 
-
-QString ProtocolHandler::filterUmlauts(QString qs_text)
-{
-MainFrame::vdcdebug("ProtocolHandler","filterUmlauts", "QString qs_text");
-
-   qs_text.replace(QChar(129), QString::fromUtf8("ü")); 
-   qs_text.replace(QChar(130), QString::fromUtf8("é")); 
-   qs_text.replace(QChar(131), QString::fromUtf8("â")); 
-   qs_text.replace(QChar(132), QString::fromUtf8("ä")); 
-   qs_text.replace(QChar(133), QString::fromUtf8("à")); 
-   qs_text.replace(QChar(134), QString::fromUtf8("å")); 
-   qs_text.replace(QChar(135), QString::fromUtf8("ç")); 
-   qs_text.replace(QChar(136), QString::fromUtf8("ê")); 
-   qs_text.replace(QChar(137), QString::fromUtf8("ë")); 
-   qs_text.replace(QChar(138), QString::fromUtf8("è")); 
-   qs_text.replace(QChar(139), QString::fromUtf8("ï")); 
-   qs_text.replace(QChar(140), QString::fromUtf8("î")); 
-   qs_text.replace(QChar(141), QString::fromUtf8("ì"));
-   qs_text.replace(QChar(142), QString::fromUtf8("Ä"));
-
-
-   qs_text.replace(QChar(145), QString::fromUtf8("æ")); 
-   qs_text.replace(QChar(146), QString::fromUtf8("Æ")); 
-   qs_text.replace(QChar(147), QString::fromUtf8("ô")); 
-   qs_text.replace(QChar(148), QString::fromUtf8("ö")); 
-   qs_text.replace(QChar(149), QString::fromUtf8("ò")); 
-   qs_text.replace(QChar(150), QString::fromUtf8("û"));
-   qs_text.replace(QChar(153), QString::fromUtf8("Ö"));
-   qs_text.replace(QChar(154), QString::fromUtf8("Ü"));
-   qs_text.replace(QChar(225), QString::fromUtf8("ß"));
-   qs_text.replace(QChar(245), QString::fromUtf8("ä"));
-
-   return qs_text;
-}
 DebugWindow::DebugWindow(QWidget *parent) : QDialog(parent)
 {
 MainFrame::vdcdebug("DebugWindow","DebugWindow", "QWidget *parent");
