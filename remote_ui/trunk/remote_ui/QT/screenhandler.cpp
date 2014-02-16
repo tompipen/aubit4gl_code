aeacd50690ab73770a15e6f51d34becfc54ac501
dkroker <dkroker@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2014-02-16 09:17:19 +0000

    set parent to ringmenu

--- remote_ui/trunk/remote_ui/QT/screenhandler.cpp
+++ remote_ui/trunk/remote_ui/QT/screenhandler.cpp
@@ -859,7 +859,7 @@ MainFrame::vdcdebug("ScreenHandler","createDialog", "QString title, QString comm
       return;
 
    //Dialog *p_dialog = new Dialog(title, comment, style, image, p_fglform);
-   RingMenuPulldown *pulldown = new RingMenuPulldown(title, comment, style, image); //, p_fglform);
+   RingMenuPulldown *pulldown = new RingMenuPulldown(title, comment, style, image, p_fglform);
    pulldown->setStyleSheet(p_fglform->styleSheet());
    //connect(pulldown, SIGNAL(clicked()), p_fglform, SLOT(close()));
    p_fglform->setRingMenuPulldown(pulldown);
