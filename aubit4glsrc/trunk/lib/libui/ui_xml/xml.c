538eb1c1a37deb7da90c1b73b16603eb7645f2bf
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2014-03-21 13:11:37 +0000

    *** empty log message ***

--- aubit4glsrc/trunk/lib/libui/ui_xml/xml.c
+++ aubit4glsrc/trunk/lib/libui/ui_xml/xml.c
@@ -1126,7 +1126,7 @@ get_field_codes (char *fields)
       *k = 0;
       k += 2;
       xcnt++;
-      x = realloc (x, sizeof (int) * xcnt);
+      x = realloc (x, sizeof (char *) * xcnt);
       p = strchr (k1, ',');
       if (p)
 	*p = 0;
