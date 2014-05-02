dd87540cdd25aef8b3da75da3aae16db593227b5
dkroker <dkroker@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2014-05-02 08:03:48 +0000

    .

--- remote_ui/trunk/remote_ui/QT/screenhandler.cpp
+++ remote_ui/trunk/remote_ui/QT/screenhandler.cpp
@@ -142,7 +142,7 @@ MainFrame::vdcdebug("ScreenHandler","~ScreenHandler", "");
     }
 
 
-    //QApplication::processEvents();
+    QApplication::processEvents();
 }
 
 //------------------------------------------------------------------------------
