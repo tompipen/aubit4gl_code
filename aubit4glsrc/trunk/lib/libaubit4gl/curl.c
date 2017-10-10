94ff9bce5639efa5f6096254f620dfa880ab4c9e
mikeaubury <mikeaubury@ccff5c2d-aaaf-4f61-a5bd-d7f44fd75822>
2017-10-10 06:48:54 +0000

    Fix for hanging curl request

--- aubit4glsrc/trunk/lib/libaubit4gl/curl.c
+++ aubit4glsrc/trunk/lib/libaubit4gl/curl.c
@@ -128,14 +128,98 @@ write_callback (char *buffer, size_t size, size_t nitems, void *userp)
 
   return size;
 }
-
 /* use to attempt to fill the read buffer up to requested number of bytes */
-static int
-fill_buffer (URL_FILE * file, int want, int waittime)
+static int fill_buffer(URL_FILE *file, int want,int n)
 {
   fd_set fdread;
   fd_set fdwrite;
   fd_set fdexcep;
+  struct timeval timeout;
+  int rc;
+  CURLMcode mc; /* curl_multi_fdset() return code */
+
+  /* only attempt to fill buffer if transactions still running and buffer
+ *  *    * doesn't exceed required size already
+ *   *       */
+  if((!file->still_running) || (file->buffer_pos > want))
+    return 0;
+
+  /* attempt to fill buffer */
+  do {
+    int maxfd = -1;
+    long curl_timeo = -1;
+
+    FD_ZERO(&fdread);
+    FD_ZERO(&fdwrite);
+    FD_ZERO(&fdexcep);
+
+    /* set a suitable timeout to fail on */
+    timeout.tv_sec = 60; /* 1 minute */
+    timeout.tv_usec = 0;
+
+    curl_multi_timeout(multi_handle, &curl_timeo);
+    if(curl_timeo >= 0) {
+      timeout.tv_sec = curl_timeo / 1000;
+      if(timeout.tv_sec > 1)
+        timeout.tv_sec = 1;
+      else
+        timeout.tv_usec = (curl_timeo % 1000) * 1000;
+    }
+
+    /* get file descriptors from the transfers */
+    mc = curl_multi_fdset(multi_handle, &fdread, &fdwrite, &fdexcep, &maxfd);
+
+    if(mc != CURLM_OK) {
+      fprintf(stderr, "curl_multi_fdset() failed, code %d.\n", mc);
+      break;
+    }
+
+   /* On success the value of maxfd is guaranteed to be >= -1. We call
+ *  *        select(maxfd + 1, ...); specially in case of (maxfd == -1) there are
+ *   *               no fds ready yet so we call select(0, ...) --or Sleep() on Windows--
+ *    *                      to sleep 100ms, which is the minimum suggested value in the
+ *     *                             curl_multi_fdset() doc. */
+
+    if(maxfd == -1) {
+#ifdef _WIN32
+      Sleep(100);
+      rc = 0;
+#else
+      /* Portable sleep for platforms other than Windows. */
+      struct timeval wait = { 0, 100 * 1000 }; /* 100ms */
+      rc = select(0, NULL, NULL, NULL, &wait);
+#endif
+    }
+    else {
+      /* Note that on some platforms 'timeout' may be modified by select().
+ *  *          If you need access to the original value save a copy beforehand. */
+      rc = select(maxfd + 1, &fdread, &fdwrite, &fdexcep, &timeout);
+    }
+
+    switch(rc) {
+    case -1:
+      /* select error */
+      break;
+
+    case 0:
+    default:
+      /* timeout or readable/writable sockets */
+      curl_multi_perform(multi_handle, &file->still_running);
+      break;
+    }
+  } while(file->still_running && (file->buffer_pos < want));
+  return 1;
+}
+
+
+/* use to attempt to fill the read buffer up to requested number of bytes */
+static int
+xfill_buffer (URL_FILE * file, int want, int waittime)
+{
+  fd_set fdread;
+  fd_set fdwrite;
+  fd_set fdexcep;
+  
   int maxfd;
   struct timeval timeout;
   int rc;
@@ -154,7 +238,7 @@ fill_buffer (URL_FILE * file, int want, int waittime)
       FD_ZERO (&fdexcep);
 
       /* set a suitable timeout to fail on */
-      timeout.tv_sec = 60;	/* 1 minute */
+      timeout.tv_sec = 1;	/* 1 minute */
       timeout.tv_usec = 0;
 
       /* get file descriptors from the transfers */
@@ -263,6 +347,13 @@ url_fopen (char *url, const char *operation)
       curl_easy_setopt (file->handle.curl, CURLOPT_URL, url);
       curl_easy_setopt (file->handle.curl, CURLOPT_WRITEDATA, file);
       curl_easy_setopt (file->handle.curl, CURLOPT_VERBOSE, FALSE);
+
+
+ //curl_easy_setopt (file->handle.curl,CURLOPT_CONNECTTIMEOUT_MS,100);
+ //curl_easy_setopt (file->handle.curl,CURLOPT_DNS_CACHE_TIMEOUT,100);
+ //curl_easy_setopt (file->handle.curl,CURLOPT_FTP_RESPONSE_TIMEOUT,100);
+ //curl_easy_setopt (file->handle.curl,CURLOPT_TIMEOUT_MS,100);
+
       curl_easy_setopt (file->handle.curl, CURLOPT_WRITEFUNCTION, write_callback);
 
       if (decompress_when_possible)
